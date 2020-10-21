3a2fcb039d6a8ce3fb45369804959b89cc943a2b
Asri Rahma <rahmawatiasri0@gmail.com>
2020-10-21 16:50:33 +0700

    create assigment for meet 1

---
+++ Meet1/Kotlin_Fundamental/src/com/asri/ElvisOperator.kt
@@ -0,0 +1,11 @@
+package com.asri
+
+fun main() {
+    val text: String? = null
+    val textLength = text?.length ?: 7
+
+    val score:Int? = null
+
+    println(textLength)
+    print(score?:89)
+}
