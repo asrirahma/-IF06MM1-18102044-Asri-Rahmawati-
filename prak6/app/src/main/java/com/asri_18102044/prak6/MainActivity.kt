ccf66daf375e1bab0c4ba9e9c13b96e28ca4b8eb
Asri Rahma <rahmawatiasri0@gmail.com>
2020-11-16 23:28:53 +0700

    praktikum 6 - running 2

--- prak6/app/src/main/java/com/asri_18102044/prak6/MainActivity.kt
+++ prak6/app/src/main/java/com/asri_18102044/prak6/MainActivity.kt
@@ -2,7 +2,11 @@ package com.asri_18102044.prak6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.asri_18102044.prak6.adapter.GridMyDataAdapter
 import com.asri_18102044.prak6.adapter.ListMyDataAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -41,5 +45,33 @@ class MainActivity : AppCompatActivity() {
         rv_mydata.layoutManager = LinearLayoutManager(this)
         val listMyDataAdapter = ListMyDataAdapter(list)
         rv_mydata.adapter = listMyDataAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showRecyclerGrid() {
+        rv_mydata.layoutManager = GridLayoutManager(this, 2)
+        val gridMyDataAdapter = GridMyDataAdapter(list)
+        rv_mydata.adapter = gridMyDataAdapter
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+            R.id.action_grid -> {
+                showRecyclerGrid()
+            }
+            R.id.action_cardview -> {
+            }
+        }
     }
 }
